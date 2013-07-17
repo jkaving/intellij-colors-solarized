482c88fcaa013dcf6d98eb5b48f29428c3330ef6
Alex Vasilenko <aa.vasilenko@gmail.com>
2013-07-18 00:34:56 +0300

    copy to both icls and xml formats

--- buildjar.bat
+++ buildjar.bat
@@ -1,6 +1,8 @@
 mkdir .\colors
 copy "Solarized Light.xml" .\colors
 copy "Solarized Dark.xml" .\colors
+copy "Solarized Light.xml" ".\colors\Solarized Light.icls"
+copy "Solarized Dark.xml" ".\colors\Solarized Dark.icls"
 copy /y nul .\"IntelliJ IDEA Global Settings"
 
 jar cfM settings.jar "IntelliJ IDEA Global Settings" .\colors

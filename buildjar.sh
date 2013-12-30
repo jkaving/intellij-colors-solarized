95d2492b9fb7149ceebc612cf2fc18a85c203241
kaving <johan.kaving@symsoft.com>
2013-12-30 23:38:03 +0100

    Rename files to ”.icls”
    The color scheme files now include ”Language Defaults”
    settings so the file names are changed to use the new ”.icls” suffix.
    The ”buildjar.sh” and ”buildjar.bat” scripts are updated to
    copy the files to versions named ”.xml” to maintain backwards
    compatibility with IntelliJ IDEA versions prior to 12.1.

--- buildjar.sh
+++ buildjar.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 mkdir colors
-cp Solarized\ Light.xml colors
-cp Solarized\ Dark.xml colors
-cp Solarized\ Light.xml colors/Solarized\ Light.icls
-cp Solarized\ Dark.xml colors/Solarized\ Dark.icls
+cp Solarized\ Light.icls colors
+cp Solarized\ Dark.icls colors
+cp Solarized\ Light.icls colors/Solarized\ Light.xml
+cp Solarized\ Dark.icls colors/Solarized\ Dark.xml
 touch IntelliJ\ IDEA\ Global\ Settings
 
 jar cfM settings.jar IntelliJ\ IDEA\ Global\ Settings colors

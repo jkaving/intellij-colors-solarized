fb8946757a2696be67d5dd27489a8ff7312aee4e
Johan Kaving <johan.kaving@symsoft.com>
2014-09-09 08:32:04 +0200

    Tweak portable sed syntax
    Use options that work on both Ubuntu and OS X.
    Specify a suffix for the backup files created
    by inline editing, and delete the backup files
    before creating the JAR file.

--- buildjar.sh
+++ buildjar.sh
@@ -9,13 +9,13 @@ function createDarculaVersion {
   cp "${1}" "${2}"
 
   # Append " (Darcula)" to the name of the scheme
-  sed -i -e 's/\(<scheme name="[^"]*\)\(" .*\)/\1 \(Darcula\)\2/' "${2}"
+  sed -i'.orig' -e 's/\(<scheme name="[^"]*\)\(" .*\)/\1 \(Darcula\)\2/' "${2}"
 
   # Remove any existing FILESTATUS elements
-  sed -i -e '/<option name="FILESTATUS_/d' "${2}"
+  sed -i'.orig' -e '/<option name="FILESTATUS_/d' "${2}"
 
   # Insert the FILESTATUS colors from the original Darcula scheme
-  sed -i -e '/<option name="CONSOLE_BACKGROUND_KEY/a\
+  sed -i'.orig' -e '/<option name="CONSOLE_BACKGROUND_KEY/a\
 \    <option name="FILESTATUS_ADDED" value="629755" />\
 \    <option name="FILESTATUS_DELETED" value="6c6c6c" />\
 \    <option name="FILESTATUS_IDEA_FILESTATUS_DELETED_FROM_FILE_SYSTEM" value="6c6c6c" />\
@@ -33,6 +33,8 @@ function createDarculaVersion {
 \    <option name="FILESTATUS_changelistConflict" value="d5756c" />\
 \    <option name="FILESTATUS_modifiedOutside" value="6897bb" />\
 ' "${2}"
+  
+  rm "${2}.orig"
 }
 
 # Create the "colors" directory for the scheme files

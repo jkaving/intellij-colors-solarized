7d0f2959ad20697b8701e3dfe6757ae0683b4293
Huy Tran <huy.tran@gapt.vn>
2015-02-25 11:16:42 -0800

    add script to modify the color for RGB on Mac

---
+++ java1.6_mac.sh
@@ -0,0 +1,17 @@
+sed -i -- 's/2b36/22029/g' *.icls   #base03
+sed -i -- 's/73642/a2933/g' *.icls  #base02
+sed -i -- 's/586e75/475b62/g' *.icls  #base01
+sed -i -- 's/657b83/536870/g' *.icls  #base00
+sed -i -- 's/839496/708284/g' *.icls  #base0
+sed -i -- 's/93a1a1/819090/g' *.icls  #base1
+sed -i -- 's/eee8d5/eae3cb/g' *.icls  #base2
+sed -i -- 's/fdf6e3/fcf4dc/g' *.icls  #base3
+sed -i -- 's/b58900/a57705/g' *.icls  #yellow
+sed -i -- 's/cb4b16/bd3613/g' *.icls  #orange
+sed -i -- 's/dc322f/d11c24/g' *.icls  #red
+sed -i -- 's/d33682/c61c6f/g' *.icls  #magenta
+sed -i -- 's/6c71c4/595ab7/g' *.icls  #violet
+sed -i -- 's/268bd2/2176c7/g' *.icls  #blue
+sed -i -- 's/2aa198/259286/g' *.icls  #cyan
+sed -i -- 's/859900/738a04/g' *.icls  #green
+
